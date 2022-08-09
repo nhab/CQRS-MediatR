dada1e336d90f1638b619c602caa7280b94569d5
nhab <n.habibollahi@yahoo.com>
2022-08-09 21:39:38 +0430

    what happens behind the scenes is that when we invoke the Send method with the request as GetEmployeeListQuery, the mediator will invoke the handler which accept this request. In our case it will go and call the GetEmployeeListHandler class and returns the response as List<EmployeeModel>.

---
+++ EmployeeManagement.API/Controllers/EmployeesController.cs
@@ -0,0 +1,24 @@
+﻿using EmployeeManagementLibrary.Models;
+using EmployeeManagementLibrary.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public EmployeesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        public async Task<List<EmployeeModel>> Get()
+        { 
+        return await _mediator.Send(new GetEmployeeListQuery());
+        }
+    }
+}
